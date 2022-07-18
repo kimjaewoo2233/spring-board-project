96e29e59592d61ae8d8b509882376b6f3c461c81
Jae woo kim <ta3844@naver.com>
2022-07-18 18:45:29 +0900

    #1 - querydsl 지원을 이용하여 api 검색옵션추가
    QuerydslPredicateExecuter,QuerydslBinderCutomizer 그리고 테스트하기 위한 데이터 data.sql도 수정

--- spring-project/src/main/java/com/example/springproject/repository/ArticleRepository.java
+++ spring-project/src/main/java/com/example/springproject/repository/ArticleRepository.java
@@ -23,9 +23,11 @@ public interface ArticleRepository extends
         //java 8 부터 인터페이스에 구현이 가능해짐
         bindings.excludeUnlistedProperties(true);   //선택적 검사 -리스팅을 하지않은 프로퍼티는 검색에서 제외
         bindings.including(root.title,root.content,root.hashtag,root.createdAt,root.createdBy);  //이 프로퍼티로만 검색이 가능하게 만든다
-     //   bindings.bind(root.title).first(StringExpression::likeIgnoreCase);   //like '${v}'  %는 내가 넣은 장소를 정함
+     //  bindings.bind(root.title).first(StringExpression::likeIgnoreCase);   //like '${v}'  %는 내가 넣은 장소를 정함
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  //like '${%v%}' 둘이 이 차이다    --문자열이라 StringExpression
         //이그젝트 매치로 동작하는데 방식을 바꾼다. 검색 파라미터는 하나만 받는다(first).
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);        //동일검사
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
--- spring-project/src/main/resources/data.sql
+++ spring-project/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into article values (1,now(),'uno,',now(),'uno','content','#tag','title');
-insert into article values (2,now(),'uno,',now(),'uno','content','#tag','title');
-insert into article values (3,now(),'uno,',now(),'uno','content','#tag','title');
-insert into article values (4,now(),'uno,',now(),'uno','content','#tag','title');
-insert into article values (5,now(),'uno,',now(),'uno','content','#tag','title');
-insert into article values (6,now(),'uno,',now(),'uno','content','#tag','title');
+insert into article values (2,now(),'uno,',now(),'uno','content','#tag','title1');
+insert into article values (3,now(),'uno,',now(),'uno','content','#tag','title2');
+insert into article values (4,now(),'uno,',now(),'uno','content','#tag','title3');
+insert into article values (5,now(),'uno,',now(),'uno','content','#tag','title4');
+insert into article values (6,now(),'uno,',now(),'uno','content','#tag','title5');
 
